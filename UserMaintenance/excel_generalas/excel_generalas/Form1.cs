ceae853c3bf74d85bc3c453300b75bec987a7067
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-10-03 11:36:43 +0200

    Excel munkafüzet létrehozása és megnyitása

--- UserMaintenance/excel_generalas/excel_generalas/Form1.cs
+++ UserMaintenance/excel_generalas/excel_generalas/Form1.cs
@@ -18,10 +18,42 @@ namespace excel_generalas
 
         RealEstateEntities context = new RealEstateEntities();
 
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
         private void LoadData()
