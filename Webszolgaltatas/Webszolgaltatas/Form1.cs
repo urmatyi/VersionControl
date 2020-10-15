1dda78c076ff9a6a456684c250657a397f24cc87
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-10-15 12:48:05 +0200

    Árfolyam lista létrehozása

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgaltatas.Entities;
+using Webszolgaltatas.MnbServiceReference;
 
 namespace Webszolgaltatas
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates;
+        
+
         public Form1()
         {
             InitializeComponent();
+
+            GerExchangeRates();
+
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void GerExchangeRates()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
