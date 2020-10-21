a2f1553839cd6183d59c5a781d3520d784c21e54
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-10-21 16:31:49 +0200

    Véletlenszám generálás

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -14,6 +14,9 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
+
+
         public Form1()
         {
             InitializeComponent();
