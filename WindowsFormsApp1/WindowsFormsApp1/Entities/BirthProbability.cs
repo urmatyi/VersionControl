bb9971272e88ba0781b4d0ed83657a8d47884458
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-10-21 15:56:49 +0200

    Osztályok létrehozása

---
+++ WindowsFormsApp1/WindowsFormsApp1/Entities/BirthProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+
+        public int NbrOfChildren { get; set; }
+
+        public double BirthP { get; set; }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Entities/DeathProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public double DeathP { get; set; }
+    }
+}
