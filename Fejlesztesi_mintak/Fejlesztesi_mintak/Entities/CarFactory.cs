5535b9c6a35cbd7ab15a9504a77e95bd088271cf
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-11-15 13:45:17 +0100

    Absztrakció felhasználása a gyárakban

--- Fejlesztesi_mintak/Fejlesztesi_mintak/Entities/CarFactory.cs
+++ Fejlesztesi_mintak/Fejlesztesi_mintak/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Fejlesztesi_mintak.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
