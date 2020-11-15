6e8ee8e16d696af578ed7f7277311188eeef1022
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-11-15 13:18:32 +0100

    Játék absztrakció kezelése

--- Fejlesztesi_mintak/Fejlesztesi_mintak/Form1.cs
+++ Fejlesztesi_mintak/Fejlesztesi_mintak/Form1.cs
@@ -44,7 +44,7 @@ namespace Fejlesztesi_mintak
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
