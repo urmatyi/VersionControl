a3ee216a9d8bb43805fc17d4de84ff44e925bb53
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-11-20 14:40:53 +0100

    E-mail validálás tesztesetek

--- UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -11,7 +11,13 @@ namespace UnitTestExample.Test
     
     public class AccountControllerTestFixture
     {
-        [Test]
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
