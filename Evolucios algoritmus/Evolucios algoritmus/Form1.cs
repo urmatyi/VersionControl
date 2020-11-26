dde9f14b2e528b57d76d72f7498cf081ba545a34
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-11-26 12:23:15 +0100

    Induló populáció felépítése

--- Evolucios algoritmus/Evolucios algoritmus/Form1.cs
+++ Evolucios algoritmus/Evolucios algoritmus/Form1.cs
@@ -16,14 +16,23 @@ namespace Evolucios_algoritmus
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
-            gc.Start(true);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+             gc.Start();
         }
     }
 }
