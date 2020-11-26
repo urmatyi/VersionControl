36fa7146abb23c8f1e94320275324fad14631165
Mátyás Úr <matyas.ur@stud.uni-corvinus.hu>
2020-11-26 12:11:31 +0100

    Játéktér

--- Evolucios algoritmus/Evolucios algoritmus/Form1.cs
+++ Evolucios algoritmus/Evolucios algoritmus/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Evolucios_algoritmus
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
         public Form1()
         {
             InitializeComponent();
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
         }
     }
 }
